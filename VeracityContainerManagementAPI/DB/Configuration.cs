8bfaee4705537040e8012b07f051732eeaed4c4d
shad.khel <shad.khel@gmail.com>
2017-11-26 21:07:00 +0100

    EF DB modification

---
+++ VeracityContainerManagementAPI/DB/Configuration.cs
@@ -0,0 +1,32 @@
+namespace VeracityContainerManagementAPI.DB
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VeracityContainerManagementAPI.DB.DataModel>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"DB";
+        }
+
+        protected override void Seed(VeracityContainerManagementAPI.DB.DataModel context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
